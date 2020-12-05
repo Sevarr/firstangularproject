052746d7c540a9823314f1c1fbe4f0d29d3522ed
Sevarr <przeliorz.sebastian@gmail.com>
2020-12-05 14:43:40 +0100

    Login v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {LoginComponent} from './login/login.component';
+
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot([
+    { path: "login", component: LoginComponent }
+])
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
