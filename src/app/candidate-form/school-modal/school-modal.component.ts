ea6932bf1161c29f8b11e5362eb7892e75d6940b
Sevarr <przeliorz.sebastian@gmail.com>
2020-12-17 01:12:32 +0100

    candidate form test

---
+++ src/app/candidate-form/school-modal/school-modal.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-school-modal',
+  templateUrl: './school-modal.component.html',
+  styleUrls: ['./school-modal.component.css']
+})
+export class SchoolModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  // open(content) {
+  //   this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+  //     this.closeResult = `Closed with: ${result}`;
+  //   }, (reason) => {
+  //     this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+  //   });
+  // }
+  //
+  // private getDismissReason(reason: any): string {
+  //   if (reason === ModalDismissReasons.ESC) {
+  //     return 'by pressing ESC';
+  //   } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //     return 'by clicking on a backdrop';
+  //   } else {
+  //     return `with: ${reason}`;
+  //   }
+  // }
+}
