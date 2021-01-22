91ac09dfdf5baf6d492b27c5214952fb4a5522d1
Sevarr <przeliorz.sebastian@gmail.com>
2021-01-22 01:02:53 +0100

    Repairing db connection.

---
+++ src/app/dialog/accounts/create-new-account/create-new-account.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { RestService } from '../../../services/rest/rest.service';
+
+@Component({
+  selector: 'app-create-new-account',
+  templateUrl: './create-new-account.component.html',
+  styleUrls: ['./create-new-account.component.css']
+})
+export class CreateNewAccountComponent implements OnInit {
+
+  constructor(private restService: RestService) { this.generateRegisterLink(); }
+
+  ngOnInit(): void {
+  }
+
+  generateRegisterLink(){
+    console.log('Testuje');
+    console.log(this.restService.newAccountRegistrationLink('WOKER'));
+
+  }
+
+}
---
+++ src/app/dialog/accounts/register-new-account/register-new-account.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterNewAccountComponent } from './register-new-account.component';
+
+describe('RegisterNewAccountComponent', () => {
+  let component: RegisterNewAccountComponent;
+  let fixture: ComponentFixture<RegisterNewAccountComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegisterNewAccountComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterNewAccountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
